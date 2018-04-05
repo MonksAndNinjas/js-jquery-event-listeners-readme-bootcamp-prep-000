8b992848f65a778a629dd1026305048a8b2b8a6a
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-05 17:57:56 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -21,7 +21,7 @@ function pressIt() {
 
 function submitIt() {
   $('form').on('submit', function() {
-    if ($( "input:first").val())
+    if ($( "input:f).val())
     alert('Your form is going to be submitted now.')
     return
   })
