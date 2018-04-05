41879c31e211c1b6e464540aefbe2dac864afa51
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-05 17:56:08 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -23,6 +23,7 @@ function submitIt() {
   $('form').on('submit', function() {
     if ($( "input:first").val() === "submit")
     alert('Your form is going to be submitted now.')
+    return
   })
 }
 
